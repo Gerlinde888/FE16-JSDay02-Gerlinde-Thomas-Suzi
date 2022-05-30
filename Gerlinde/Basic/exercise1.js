33fd097c87f81e54342e2cc68b1a30f7ab6537f2
Gerlinde <gerlinde@sandmair.com>
2022-05-30 14:23:33 +0200

    exercise4

---
+++ Gerlinde/Basic/exercise1.js
@@ -0,0 +1,25 @@
+// (()=>console.log('JS Connected'))();
+// const crystalGazer = (number_children, partners_name, geo_location, job_titel) =>
+// {
+//     number_children = "2";
+//     partners_name = "Tom";
+//     geo_location = "Steyr";
+//     job_title = "Boss";
+// }
+
+// console.log(crystalGazer(number_children, partners_name, geo_location, job_title));
+
+
+// function crystalGazer (number_childrens, partners_name, geo_location, job_title) {
+//     return "You will be a " + job_title + " in " + geo_location + " and married to " + partners_name + " with " + number_childrens;
+// }
+// console.log(crystalGazer (2, "Tom", "Steyr", "Boss"));
+
+(() => console.log("JS Connected"))();
+
+const crystalGazer = (number_childrens, partners_name, geo_location, job_title) => 
+
+       "You will be a " + job_title + " in " + geo_location + " and married to " + partners_name + " with " + number_childrens;
+
+       console.log(crystalGazer(2, "Tom", "Steyr", "Boss"));
+
---
+++ Gerlinde/Basic/exercise2.js
@@ -0,0 +1,34 @@
+// Age Calculator
+
+// (() => console.log("JS Connected"))();
+
+// (() => console.log("JS Connected"))();
+
+
+// const ageCalculator = (birth_year, current_year) => 
+
+// let now = new Date();
+// console.log(now.getDate());
+
+(() => console.log("JS Connected"))();
+
+const ageCalculator2 = (birth_year, current_year) => 
+
+{
+    result1 = current_year - birth_year;
+    result2 = result1 - 1;
+    return `You are either ${result1} or ${result2}`;}
+
+console.log(ageCalculator2(1964, 2022));
+
+
+
+// const ageCalculator3 = (birth_year) => 
+// {
+//     let now = new Date();
+//     let current_year = (now.getFullYear());
+//     result1 = current_year - birthday;
+//     result2 = result1 - 1;
+// return "You are either ${result1} or ${result2}";}
+
+// console.log(ageCalculator3(1964 ));
