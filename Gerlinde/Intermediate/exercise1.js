5fbfbbf444f2a05272dde2023bd3d91bacaa35ad
Gerlinde <gerlinde@sandmair.com>
2022-05-30 14:58:53 +0200

    intermediate exercises

--- Gerlinde/Intermediate/exercise1.js
+++ Gerlinde/Intermediate/exercise1.js
@@ -1,1 +1,11 @@
+// Capitalize
 
+
+(() => console.log("JS Connected"))();
+
+const capFirstletter = (string) => 
+{
+       let firstletter = string[0].toUpperCase();
+       console.log(firstletter + string.slice(1));
+    }
+    capFirstletter("i am a web developer");
